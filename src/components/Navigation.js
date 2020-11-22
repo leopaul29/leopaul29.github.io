281ba555105633015b8515b2d18be2c333c1b4fa
Leo-paul <leopaulmartin@hotmail.fr>
2020-11-22 01:08:39 +0100

    fix navigation warning

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,22 @@
+import { Link } from 'gatsby'
 import React from 'react'
 
 export default function Navigation() {
     return (
         <nav class="navbar" role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
-                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                </a>
+                <Link role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </Link>
             </div>
 
             <div id="navbarBasicExample" class="navbar-menu">
                 <div class="navbar-end">
-                    <a class="navbar-item">Works</a>
-                    <a class="navbar-item">Blog</a>
-                    <a class="navbar-item">Contact</a>
+                    <Link class="navbar-item">Works</Link>
+                    <Link class="navbar-item">Blog</Link>
+                    <Link class="navbar-item">Contact</Link>
                 </div>
             </div>
         </nav>
