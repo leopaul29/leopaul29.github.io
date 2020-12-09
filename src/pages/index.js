5544a209cf1ba61eadbb929414426d9f19ba1d74
Leo-paul <leopaulmartin@hotmail.fr>
2020-12-09 23:41:25 +0100

    remove workstimeline from index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,12 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import PresentationContainer from "../Container/PresentationContainer"
-import WorksTimeLineContainer from "../Container/WorksTimeLineContainer"
 
 const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
       <PresentationContainer />
-      <WorksTimeLineContainer />
     </Layout>
   )
 }
