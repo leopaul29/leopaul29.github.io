e2fdffd7e56dce5cd5b9005b6d3017f2bd605c87
Leo-paul <leopaulmartin@hotmail.fr>
2020-12-12 23:12:15 +0100

    workstimeline prettifier

--- src/components/WorksTimeLine.js
+++ src/components/WorksTimeLine.js
@@ -1,14 +1,16 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-export const WorksTimeLine = ({ startdate, title, timelineclass, imageSource }) => (
+export const WorksTimeLine = ({
+  startdate,
+  title,
+  timelineclass,
+  imageSource,
+}) => (
   <div className={`timeline-item ${timelineclass}`}>
     <div className={`timeline-marker ${timelineclass}`}></div>
     <div className={`timeline-marker ${timelineclass} is-image is-48x48`}>
-      <img
-        src={imageSource}
-        alt="placeholders"
-      />
+      <img src={imageSource} alt="placeholders" />
     </div>
     <div className="timeline-content">
       <p className="heading">{startdate}</p>
@@ -21,4 +23,5 @@ WorksTimeLine.propTypes = {
   startdate: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   timelineclass: PropTypes.string.isRequired,
+  imageSource: PropTypes.string,
 }
