c07522757f98d8201d952b07a644e2d4924f33f1
Leo-paul <leopaulmartin@hotmail.fr>
2020-12-17 14:48:35 +0100

    add fontawesome lib in app

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import PresentationContainer from "../Container/PresentationContainer"
 import ProjectsContainer from "../Container/ProjectsContainer"
+// import fontawesome icons
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+library.add(fab)
 
 const IndexPage = () => {
   return (
