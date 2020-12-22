1dfd40428621d8543476e84fa14e55049f8f8c10
Leo-paul <leopaulmartin@hotmail.fr>
2020-12-22 22:30:25 +0100

    setup 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,33 @@
 import React from "react"
-
-import Layout from "../components/layout"
+import { Link } from "gatsby"
 import SEO from "../components/seo"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons"
 
+const arrowLeftIcon = (
+  <span class="icon mr-3 is-large ">
+    <FontAwesomeIcon icon={faArrowLeft} size="lg" />
+  </span>
+)
 const NotFoundPage = () => (
-  <Layout>
+  <>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <section className="hero is-danger is-fullheight">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1 className="title is-1">404: Not Found</h1>
+          <p className="subtitle">
+            You just hit a route that doesn&#39;t exist... the sadness.
+          </p>
+          <Link to="/">
+            <button className="button is-info is-rounded">
+              {arrowLeftIcon}Go back to the homepage
+            </button>
+          </Link>
+        </div>
+      </div>
+    </section>
+  </>
 )
 
 export default NotFoundPage
