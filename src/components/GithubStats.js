32a76dcfc77b6c23695e83d8b66f8ea2c4be7f70
Leo-paul <leopaulmartin@hotmail.fr>
2020-12-23 23:34:24 +0100

    add github stats section

---
+++ src/components/GithubStats.js
@@ -0,0 +1,51 @@
+import React from "react"
+
+export default function GithubStats() {
+  return (
+    <section className="section has-background-info is-gradient is-rounded">
+      <div className="container">
+        <h2 className="title is-2 has-text-white-ter has-text-right-desktop">
+          My Github Stats
+          <span role="img" aria-label="emoji">
+            ✨
+          </span>{" "}
+        </h2>
+        <div className="tile is-ancestor my-5">
+          <div className="tile is-half is-vertical is-parent">
+            <div className="tile is-child">
+              <a
+                href="https://github.com/anuraghazra/github-readme-stats"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="https://github-readme-stats.vercel.app/api?username=leopaul29&hide=contribs,issues&show_icons=true"
+                  alt="Github stats"
+                />
+              </a>
+            </div>
+            <div className="tile is-child">
+              <a
+                href="https://github.com/anuraghazra/github-readme-stats"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="https://github-readme-stats.vercel.app/api/top-langs/?username=leopaul29&layout=compact&hide=C#"
+                  alt="Top Langs"
+                />
+              </a>
+            </div>
+          </div>
+          <div className="tile is-parent">
+            <div className="tile is-child">
+              <figure>
+                <embed src="https://wakatime.com/share/@0ec9f97c-0e0c-4300-b2db-01899b44cb11/527c4b9a-b96f-49ae-9bfb-91b42779cdcf.svg"></embed>
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,11 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import PresentationContainer from "../Container/PresentationContainer"
 import ProjectsContainer from "../Container/ProjectsContainer"
+import GithubStats from "../components/GithubStats"
 // import fontawesome icons
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+import { fas } from "@fortawesome/free-solid-svg-icons"
 library.add(fab, fas)
 
 const IndexPage = () => {
@@ -15,6 +16,7 @@ const IndexPage = () => {
       <SEO title="Home" />
       <PresentationContainer />
       <ProjectsContainer />
+      <GithubStats />
     </Layout>
   )
 }
