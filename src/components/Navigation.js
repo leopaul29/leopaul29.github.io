1112f04b3242d6e99e50c82673f58405f2298891
Leo-paul <leopaulmartin@hotmail.fr>
2021-03-03 22:37:16 +0100

    nav: add octocat logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,50 +1,56 @@
 import { Link } from "gatsby"
 import React from "react"
+import avatar from "../images/octocat-1607453320426.png"
 
 const Navigation = () => {
   return (
-    <nav className="navbar mt-5" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <Link
-          role="button"
-          className="navbar-burger"
-          aria-label="menu"
-          aria-expanded="false"
-          to="/"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </Link>
-      </div>
-
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-end">
-          <Link className="navbar-item" to="/#projects">
-            Projects
-          </Link>
-          {/* <Link className="navbar-item" to="/works">
-            Works
-          </Link> */}
-          <a
-            className="navbar-item"
-            href="https://lpm.hashnode.dev/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Blog
+    <nav className="navbar py-3" role="navigation" aria-label="main navigation">
+      <div className="container">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://myoctocat.com/" target="_blank" rel="noreferrer">
+            <img className="logo__img" src={avatar} alt="logo" />
           </a>
 
-          <div className="navbar-item">
-            <div className="field ">
-              <p className="control">
-                <a
-                  className="button is-link is-outlined is-rounded"
-                  href="mailto:leopaulmartin@hotmail.fr"
-                >
-                  Contact
-                </a>
-              </p>
+          <Link
+            role="button"
+            className="navbar-burger"
+            aria-label="menu"
+            aria-expanded="false"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </Link>
+        </div>
+
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-end">
+            <Link className="navbar-item" to="/#projects">
+              Projects
+            </Link>
+            {/* <Link className="navbar-item" to="/works">
+            Works
+          </Link> */}
+            <a
+              className="navbar-item"
+              href="https://lpm.hashnode.dev/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Blog
+            </a>
+
+            <div className="navbar-item">
+              <div className="field ">
+                <p className="control">
+                  <a
+                    className="button is-link is-outlined is-rounded"
+                    href="mailto:leopaulmartin@hotmail.fr"
+                  >
+                    Contact
+                  </a>
+                </p>
+              </div>
             </div>
           </div>
         </div>
