21670d8e9faa2835070a426b385be4dfbea5f32d
Leo-paul <leopaulmartin@hotmail.fr>
2021-03-03 22:37:37 +0100

    style: change presentation

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -14,63 +14,69 @@ export const Presentation = ({
   RirekishoJPfile,
   ShokumukeirekishoJPfile,
 }) => (
-  <section className="hero mt-6">
-    <div className="hero-body">
-      <div className="columns">
-        <div className="column is-3">
-          <figure className="image is-square">
-            <a href="https://myoctocat.com/" target="_blank" rel="noreferrer">
-              <img className="is-rounded" src={avatar} alt="profile_image" />
-            </a>
-          </figure>
-        </div>
-        <div className="column is-spaced">
-          <h1 className="title is-1">{title}</h1>
-          <h2 className="subtitle is-4 mt-3">{subtitle}</h2>
-          <div
-            className="content"
-            dangerouslySetInnerHTML={{ __html: htmlDescription }}
-          ></div>
-          <div class="dropdown is-hoverable">
-            <div class="dropdown-trigger">
-              <button
-                class="button is-link is-outlined is-rounded"
-                aria-haspopup="true"
-                aria-controls="dropdown-menu"
-              >
-                <span>Download my Resume</span>
-                <span className="icon is-small">
-                  <FontAwesomeIcon icon={["fas", "angle-down"]} />
-                </span>
-              </button>
-            </div>
-            <div class="dropdown-menu" id="dropdown-menu" role="menu">
-              <div class="dropdown-content">
-                <a href={CVFRfile} download={true} class="dropdown-item">
-                  CV Français
-                </a>
-                <a href={ResumeENfile} download={true} class="dropdown-item">
-                  English Resume
-                </a>
-                <hr class="dropdown-divider" />
-                <a href={RirekishoJPfile} download={true} class="dropdown-item">
-                  履歴書
-                </a>
-                <a
-                  href={ShokumukeirekishoJPfile}
-                  download={true}
-                  class="dropdown-item"
+  <section className="hero is-white has-text-centered pt-3">
+    <div className="container">
+      <div className="hero-body">
+        <div className="columns">
+          {/* <div className="column is-3">
+            <figure className="image is-square">
+              <a href="https://myoctocat.com/" target="_blank" rel="noreferrer">
+                <img className="is-rounded" src={avatar} alt="profile_image" />
+              </a>
+            </figure>
+          </div> */}
+          <div className="column has-text-centered">
+            <h1 className="title is-1">{title}</h1>
+            <h2 className="subtitle is-4 mt-3">{subtitle}</h2>
+            <div
+              className="content"
+              dangerouslySetInnerHTML={{ __html: htmlDescription }}
+            ></div>
+            <div class="dropdown is-hoverable">
+              <div class="dropdown-trigger">
+                <button
+                  class="button is-link is-outlined is-rounded"
+                  aria-haspopup="true"
+                  aria-controls="dropdown-menu"
                 >
-                  職務経歴書
-                </a>
+                  <span>Download my Resume</span>
+                  <span className="icon is-small">
+                    <FontAwesomeIcon icon={["fas", "angle-down"]} />
+                  </span>
+                </button>
+              </div>
+              <div class="dropdown-menu" id="dropdown-menu" role="menu">
+                <div class="dropdown-content">
+                  <a href={CVFRfile} download={true} class="dropdown-item">
+                    CV Français
+                  </a>
+                  <a href={ResumeENfile} download={true} class="dropdown-item">
+                    English Resume
+                  </a>
+                  <hr class="dropdown-divider" />
+                  <a
+                    href={RirekishoJPfile}
+                    download={true}
+                    class="dropdown-item"
+                  >
+                    履歴書
+                  </a>
+                  <a
+                    href={ShokumukeirekishoJPfile}
+                    download={true}
+                    class="dropdown-item"
+                  >
+                    職務経歴書
+                  </a>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div className="hero-footer m-6 ">
-      <img src={hero} alt="hero" />
+      <div className="hero-foot">
+        <img src={hero} alt="hero" />
+      </div>
     </div>
   </section>
 )
